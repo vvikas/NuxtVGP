73c04288dc4b453af17cb807fffc27a1aadb8a89
Shuban Pandita <shubanpandita@Shubans-Air.local>
2024-06-09 12:53:29 +0530

    feat: added new pages for launches, rockets, favourites

--- client/layouts/default.vue
+++ client/layouts/default.vue
@@ -1,12 +1,25 @@
 <template>
 	<v-app>
+		<div>
+			<NavBar />
+		</div>
 		<v-main>
 			<slot />
 		</v-main>
+		<div>
+			<AppFooter />
+		</div>
 	</v-app>
 </template>
 <script>
+import NavBar from './navbar.vue'
+import AppFooter from './footer.vue'
+
 export default {
 	name: 'DefaultLayout',
+	components: {
+		NavBar,
+		AppFooter,
+	},
 }
 </script>
